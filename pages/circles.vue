ace93df00a70c113bb38e23ee43ce55d19857698
mikespin <michalkrecisz@yahoo.com>
2022-08-16 21:46:45 +0200

    init repo

---
+++ pages/circles.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <div class="header">
+      <div class="logo"></div>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Next</li>
+          <li>Prev</li>
+        </ul>
+      </nav>
+    </div>
+    <div class="circle-container">
+      <div class="vertical-line"></div>
+      <div class="horizontal-line"></div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+// https://www.appart.agency/
+// https://www.youtube.com/watch?v=oYZT2VBATIo&ab_channel=ConorBailey
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'CirclesPage',
+})
+</script>
